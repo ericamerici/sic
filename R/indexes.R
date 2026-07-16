c45ec5c871bae409784f744b03c3f9c331368be9
ericamerici <ericamerici@gmail.com>
2026-07-16 12:31:54 -0500

    Update indexes.R

--- R/indexes.R
+++ R/indexes.R
@@ -1,1 +1,11 @@
-
+numeri <- function(x){
+  if(x>0){
+    print("Questo numero è positivo, ciuco!")
+    }
+  else if(x<0) {
+    print("Questo numero è negativo, se non sai queste cose, torna a scuola")  
+    }
+  else {
+    print("Zero non è né negativo né positivo") 
+    }
+  }
