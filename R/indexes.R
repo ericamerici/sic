72e62aa7ad1d97682ec72e7b27b553bf5449e312
ericamerici <ericamerici@gmail.com>
2026-07-16 14:17:28 -0500

    Update indexes.R

--- R/indexes.R
+++ R/indexes.R
@@ -1,3 +1,11 @@
+# NDMI 
+# Normalized Difference Moist Index, useful when water must be detected in the vegetation
+# How to use (an example)
+# park2025 <- rast("p25.tif")
+# ndmi2025 <- sic.ndmi(park2025, 5, 4)
+# plot(ndmi2025)
+
+
 sic.ndmi <- function(x, swir, nir) {
 
   if (!inherits(x, "SpatRaster")) {
